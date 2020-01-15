7b520047bc0dc57215b85553c1c7b06a383d92a3
Batzorig <batzorig1691@gmail.com>
2020-01-15 17:50:30 +0900

    Implement basic tests

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -4,29 +4,24 @@
 #![test_runner(oxid::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use oxid::println;
+use oxid::{println, serial_print, serial_println};
 use core::panic::PanicInfo;
 
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     oxid::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    serial_print!("test_println... ");
+    println!("test_println output");
+    serial_println!("[ok]");
 }
