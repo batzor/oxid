52b0334ab75feb70dcc2702e604725d9edfe6fe4
Batzorig Zorigoo <batzorig@kaist.ac.kr>
2020-01-15 22:48:53 +0900

    Add example interrupt handler

--- tests/interrupt_handlers.rs
+++ tests/interrupt_handlers.rs
@@ -5,30 +5,24 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use oxid::println;
+use oxid::{serial_print, serial_println};
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
     oxid::init();
-
-    #[cfg(test)]
     test_main();
-
     loop {}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     oxid::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_breakpoint_exception() {
+    serial_print!("test_breakpoint_exception...");
+    // invoke a breakpoint exception
+    x86_64::instructions::interrupts::int3();
+    serial_println!("[ok]");
 }
