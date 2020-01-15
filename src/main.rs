4ad08711b15ddde2699a2fea296d66c8105c1aa7
Batzorig <batzorig1691@gmail.com>
2020-01-15 17:11:12 +0900

    Implement minimum working kernel

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+mod vga_buffer;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello, World!");
+
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
+    loop {}
+}
